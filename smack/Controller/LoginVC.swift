25449822cfa391594820067304399beb78ec737a
Rasmus Andersson <rasmusandersson@Rasmus-Genesis-IT.local>
2018-12-09 12:46:08 +0100

    skapade login VC

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Rasmus Andersson on 2018-12-09.
+//  Copyright © 2018 Rasmus Andersson. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //MARK: Outlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    //MARK: Actions
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/Utilities/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  smack
+//
+//  Created by Rasmus Andersson on 2018-12-09.
+//  Copyright © 2018 Rasmus Andersson. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
