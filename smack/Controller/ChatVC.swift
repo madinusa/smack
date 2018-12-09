fa2bc1be084463df0b4fbfd7a4d78b5691738831
Rasmus Andersson <rasmusandersson@Rasmus-Genesis-IT.local>
2018-12-09 10:52:11 +0100

    fixade story

---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Rasmus Andersson on 2018-12-09.
+//  Copyright © 2018 Rasmus Andersson. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //MARK:  Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+}
