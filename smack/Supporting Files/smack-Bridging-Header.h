05862d33da8764dbb8568a3ce744513108296464
Rasmus Andersson <rasmusandersson@Rasmus-Genesis-IT.local>
2018-12-09 11:25:04 +0100

    end of lesson 2

--- smack/Supporting Files/smack-Bridging-Header.h
+++ smack/Supporting Files/smack-Bridging-Header.h
@@ -1,5 +1,14 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  smack-Bridging-Header.h
+//  smack
+//
+//  Created by Rasmus Andersson on 2018-12-09.
+//  Copyright © 2018 Rasmus Andersson. All rights reserved.
 //
 
+#ifndef smack_Bridging_Header_h
+#define smack_Bridging_Header_h
+
+
+#endif /* smack_Bridging_Header_h */
 #import "SWRevealViewController.h"
